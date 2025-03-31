8dbd863c4048c0120cc1aab5875f573d15d66661
Haskely <Haskely@live.com>
2025-03-31 14:41:23 +0800

    refactor(adaptio.__init__)：在 `__init__.py` 中添加 `raise_on_overload` 装饰器的导入，以支持服务过载错误处理。

--- src/adaptio/__init__.py
+++ src/adaptio/__init__.py
@@ -4,6 +4,7 @@ from .adaptive_async_concurrency_limiter import (
 )
 from .adjustable_semaphore import AdjustableSemaphore
 from .raise_on_aiohttp_overload import raise_on_aiohttp_overload
+from .raise_on_overload_by_guessing import raise_on_overload
 from .with_adaptive_retry import with_adaptive_retry
 from .with_async_control import with_async_control
 
@@ -11,6 +12,7 @@ __all__ = [
     "AdaptiveAsyncConcurrencyLimiter",
     "AdjustableSemaphore",
     "raise_on_aiohttp_overload",
+    "raise_on_overload",
     "ServiceOverloadError",
     "with_adaptive_retry",
     "with_async_control",
